108f9322f6aa84f184d1c71b1e08875fc0892e36
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2024-09-28 11:23:46 -0300

    refactor: clase Arista feat: getter de probabilidad en clase Arista

--- src/logica/Arista.java
+++ src/logica/Arista.java
@@ -2,15 +2,22 @@ package logica;
 
 public class Arista {
 	@SuppressWarnings("unused")
-	private String nombreEspiaOrigen;
+	private int nombreEspiaOrigen;
 	@SuppressWarnings("unused")
-	private String nombreEspiaDestino;
-	@SuppressWarnings("unused")
-	private Integer probabilidad;
+	private int nombreEspiaDestino;
+	private int probabilidad;
 	
-	public Arista(String nombreEspiaOrigen, String nombreEspiaDestino, Integer probabilidad){
-		this.nombreEspiaOrigen=nombreEspiaOrigen;
-		this.nombreEspiaDestino=nombreEspiaDestino;
-		this.probabilidad=probabilidad;
+	public Arista(int nombreEspiaOrigen, int nombreEspiaDestino, int probabilidad) {
+		this.nombreEspiaOrigen = nombreEspiaOrigen;
+		this.nombreEspiaDestino = nombreEspiaDestino;
+		this.probabilidad = probabilidad;
 	}
+
+	public int obtenerProbabilidad() {
+		return probabilidad;
+	}
+	
+
+	
+	
 }
