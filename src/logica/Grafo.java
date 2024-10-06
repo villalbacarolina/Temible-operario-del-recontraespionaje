979953e15b56af9c3896a415ac5a07f2be6e5e51
Carolina Villalba <carolinaangelesvillalba@gmail.com>
2024-10-06 20:04:44 -0300

    feat(RedEspias): testing con main

--- src/logica/Grafo.java
+++ src/logica/Grafo.java
@@ -6,32 +6,32 @@ public class Grafo implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
               //Vertice1  //Vertice2 //Peso
-    private Map<String, Map<String, Integer>> vertices;
-    private List<Arista> aristas; 
+    private Map<String, Map<String, Integer>> grafo;
+    transient private List<Arista> aristas; 
     
     public Grafo() {
-        vertices = new HashMap<>();
+        grafo = new HashMap<>();
         aristas = new ArrayList<>();
     }
 
     public Grafo(List<String> vertices) {
         this();
         for (String vertice : vertices) 
-            this.vertices.put(vertice, new HashMap<String, Integer>());
+            this.grafo.put(vertice, new HashMap<String, Integer>());
     }
   
   
     public void agregarVertice(String v) {
     	if(existeVertice(v))
     		throw new RuntimeException("El vértice '" +v+ "' ya existe.");
-    	vertices.put(v, new HashMap<String, Integer>());
+    	grafo.put(v, new HashMap<String, Integer>());
     }
 
     public void agregarArista(String vertice1, String vertice2, int peso) {
         verificarVertices(vertice1, vertice2);
 
-        vertices.get(vertice1).put(vertice2, peso);
-        vertices.get(vertice2).put(vertice1, peso);
+        grafo.get(vertice1).put(vertice2, peso);
+        grafo.get(vertice2).put(vertice1, peso);
 
         aristas.add(new Arista(vertice1, vertice2, peso));
     }
@@ -40,8 +40,8 @@ public class Grafo implements Serializable{
         existeVertice(vertice1);
         existeVertice(vertice2);
         if(existeArista(vertice1, vertice2)) {
-        	vertices.get(vertice1).remove(vertice2);
-        	vertices.get(vertice2).remove(vertice1);
+        	grafo.get(vertice1).remove(vertice2);
+        	grafo.get(vertice2).remove(vertice1);
         
         	aristas.removeIf(arista -> (arista.getOrigen().equals(vertice1) && arista.getDestino().equals(vertice2))
         							|| (arista.getOrigen().equals(vertice2) && arista.getDestino().equals(vertice1)));
@@ -52,16 +52,16 @@ public class Grafo implements Serializable{
     }
     
     public void eliminarVertice(String vertice) {
-    	vertices.remove(vertice);
+    	grafo.remove(vertice);
     }
     
     public int tamano() {
-        return vertices.size();
+        return grafo.size();
     }
     
     public String obtenerVerticeAleatorio() {
-        if (!vertices.isEmpty()) {
-        	String vertice = vertices.keySet().iterator().next();
+        if (!grafo.isEmpty()) {
+        	String vertice = grafo.keySet().iterator().next();
             return vertice;
         } 
         else 
@@ -73,7 +73,7 @@ public class Grafo implements Serializable{
     
     //Pendiente: Mejorar encapsulamiento
     public Map<String, Map<String, Integer>> obtenerVertices() {
-        return this.vertices;
+        return this.grafo;
     }
 
     //Pendiente: Mejorar encapsulamiento
@@ -84,27 +84,27 @@ public class Grafo implements Serializable{
     //Pendiente: Mejorar encapsulamiento
     public Map<String, Integer> obtenerVecinosVertice(String v) {
         if (existeVertice(v))
-            return this.vertices.get(v);
+            return this.grafo.get(v);
         throw new IllegalArgumentException("Vértice no válido: " + v);
     }
     
     public int obtenerPesoArista(String i, String j) {
         if (existeArista(i, j)) 
-            return vertices.get(i).get(j);
+            return grafo.get(i).get(j);
         throw new IllegalArgumentException("La arista no existe entre " + i + " y " + j);
     }
     
     // Auxiliares //
     
-    private boolean existeVertice(String v) {
-        if (!vertices.containsKey(v))
+    public boolean existeVertice(String v) {
+        if (!grafo.containsKey(v))
             return false;
         return true;
     }
         
-    private boolean existeArista(String i, String j) {
+    public boolean existeArista(String i, String j) {
         verificarVertices(i, j);
-        return vertices.get(i).containsKey(j);
+        return grafo.get(i).containsKey(j);
     }
     
     private void verificarVerticesDistintos(String v1, String v2) {
--- src/logica/RedEspias.java
+++ src/logica/RedEspias.java
@@ -1,78 +1,72 @@
 package logica;
 
 import java.io.Serializable;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.FileReader;
 import java.io.IOException;
-
 import java.util.HashMap;
 
-public class RedEspias implements Serializable{
-	private static final long serialVersionUID = 1L;
-	
-	private HashMap<String, Grafo> redesEspias; //nombre de la red, y sus caminos
-	private HashMap<String, Grafo> caminosMasSeguros; //nombre de la red, y su camino más seguro
-	
-	public RedEspias() {
-		redesEspias = new HashMap<String, Grafo>();
-		caminosMasSeguros = new HashMap<String, Grafo>();
-	}
-	
-	public void crearRed(String nombre){
-		redesEspias.put(nombre, new Grafo());
-		caminosMasSeguros.put(nombre, null);
-	}
-	
-	private Grafo obtenerRed(String nombreRed) {
-		if(!redesEspias.containsKey(nombreRed))
-			throw new IllegalArgumentException("La red "+nombreRed+" no existe");
-		else
-			return redesEspias.get(nombreRed);	
-	}
-	
-	public void agregarEspia(String nombreRed, String nombreEspia){
-		obtenerRed(nombreRed).agregarVertice(nombreEspia);
-	}
-	
-	public void borrarEspia(String nombreRed, String nombreEspia){
-		obtenerRed(nombreRed).eliminarVertice(nombreEspia);
-	}
-	
-	public void establecerCamino(String nombreRed, String nombreEspia1, String nombreEspia2, int probabilidadIntercepcion){
-		obtenerRed(nombreRed).agregarArista(nombreEspia1, nombreEspia2, probabilidadIntercepcion);
-	}
-	
-	public void borrarCamino(String nombreRed, String nombreEspia1, String nombreEspia2){
-		obtenerRed(nombreRed).eliminarArista(nombreEspia1, nombreEspia2);
-	}
-	
-	public void establecerCaminoMasSeguro(String nombreRed) {
-		Grafo red = obtenerRed(nombreRed);
-		caminosMasSeguros.put(nombreRed, ArbolGeneradorMinimo.obtenerAGMComoGrafo(red));
-	}
-	
-	public void guardarComoJSON(String nombreArchivo) {
+public class RedEspias implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private HashMap<String, Grafo> redesEspias; // nombre de la red, y sus caminos
+    private HashMap<String, Grafo> caminosMasSeguros; // nombre de la red, y su camino más seguro
+
+    public RedEspias() {
+        redesEspias = new HashMap<>();
+        caminosMasSeguros = new HashMap<>();
+    }
+
+    public void crearRed(String nombre) {
+        redesEspias.put(nombre, new Grafo());
+        caminosMasSeguros.put(nombre, null);
+    }
+
+    private Grafo obtenerRed(String nombreRed) {
+        if (!redesEspias.containsKey(nombreRed))
+            throw new IllegalArgumentException("La red " + nombreRed + " no existe");
+        return redesEspias.get(nombreRed);
+    }
+
+    public void agregarEspia(String nombreRed, String nombreEspia) {
+        obtenerRed(nombreRed).agregarVertice(nombreEspia);
+    }
+
+    public void borrarEspia(String nombreRed, String nombreEspia) {
+        obtenerRed(nombreRed).eliminarVertice(nombreEspia);
+    }
+
+    public void establecerCamino(String nombreRed, String nombreEspia1, String nombreEspia2, int probabilidadIntercepcion) {
+        obtenerRed(nombreRed).agregarArista(nombreEspia1, nombreEspia2, probabilidadIntercepcion);
+    }
+
+    public void borrarCamino(String nombreRed, String nombreEspia1, String nombreEspia2) {
+        obtenerRed(nombreRed).eliminarArista(nombreEspia1, nombreEspia2);
+    }
+
+    public void establecerCaminoMasSeguro(String nombreRed) {
+        Grafo red = obtenerRed(nombreRed);
+        caminosMasSeguros.put(nombreRed, ArbolGeneradorMinimo.obtenerAGMComoGrafo(red));
+    }
+
+    public void guardarComoJSON(String nombreArchivo) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(this);
 
-        String nombreCarpeta = "../informacion";
+        // Crear un directorio en la carpeta del usuario
+        String nombreCarpeta = System.getProperty("user.home") + "/informacion";
         File carpeta = new File(nombreCarpeta);
-        if (!carpeta.exists()) 
+        if (!carpeta.exists())
             carpeta.mkdirs();
 
         String rutaArchivo = nombreCarpeta + File.separator + nombreArchivo;
 
         try (FileWriter writer = new FileWriter(rutaArchivo)) {
             writer.write(json);
-            writer.close();
             System.out.println("Archivo JSON guardado en: " + rutaArchivo);
         } catch (IOException e) {
             e.printStackTrace();
@@ -84,10 +78,10 @@ public class RedEspias implements Serializable{
         Gson gson = new Gson();
         RedEspias ret = null;
 
-        //lee el archivo
+        // Lee el archivo
         try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
-        	//deserializa: pasa de .json a RedEspias
-            ret = gson.fromJson(br, RedEspias.class); 
+            // Deserializa: pasa de .json a RedEspias
+            ret = gson.fromJson(br, RedEspias.class);
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Error al leer el archivo JSON.");
@@ -98,4 +92,32 @@ public class RedEspias implements Serializable{
         return ret;
     }
 
+    public static void main(String[] args) {
+        RedEspias redEspias = new RedEspias();
+
+        redEspias.crearRed("Red1");
+        redEspias.crearRed("Red2");
+
+        redEspias.agregarEspia("Red1", "Espia1");
+        redEspias.agregarEspia("Red1", "Espia2");
+        redEspias.agregarEspia("Red1", "Espia3");
+
+
+        redEspias.establecerCamino("Red1", "Espia1", "Espia2", 50);
+        redEspias.establecerCamino("Red1", "Espia1", "Espia3", 4);
+        
+        redEspias.establecerCaminoMasSeguro("Red1");
+
+        redEspias.guardarComoJSON("redEspias.json");
+
+        // Leer el archivo JSON
+        RedEspias nuevaRedEspias = RedEspias.leerJSON(System.getProperty("user.home") + "/informacion/redEspias.json");
+
+        // Comprobar si se leyeron correctamente los datos
+        if (nuevaRedEspias != null) {
+            System.out.println("Archivo JSON leído correctamente.");
+        } else {
+            System.out.println("Error al leer el archivo JSON.");
+        }
+    }
 }
